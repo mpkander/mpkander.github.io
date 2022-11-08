9951d3f5743fc76f2c2599c4929f2fecec7cc966
Mark Abramenko <abramenko@surf.dev>
2022-11-08 20:02:04 +0600

    Update PWA

--- canvas/script/loading.js
+++ canvas/script/loading.js
@@ -1,20 +1,16 @@
 window.addEventListener('load', function (ev) {
-       let loading = document.querySelector('#loading');
+       let loading = document.querySelector('#icon');
+       let bottom = document.querySelector('#bottom');
        _flutter.loader.loadEntrypoint({
          serviceWorker: {
            serviceWorkerVersion: serviceWorkerVersion,
          }
        }).then(function (engineInitializer) {
-         loading.classList.add('main_done');
          return engineInitializer.initializeEngine();
        }).then(function (appRunner) {
-         loading.classList.add('init_done');
-         return appRunner.runApp();
-       }).then(function (app) {
-         // Wait a few milliseconds so users can see the "zoooom" animation
-         // before getting rid of the "loading" div.
-         window.setTimeout(function () {
-           loading.remove();
-         }, 800);
+        return appRunner.runApp().then(function () {
+        loading.remove();
+        bottom.remove();
+        });
        });
      });
