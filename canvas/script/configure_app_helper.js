660fd521102ebcd7f88cdfca15336f9b67abc218
Mark Abramenko <abramenko@surf.dev>
2022-11-10 23:02:08 +0600

    Update PWA

---
+++ canvas/script/configure_app_helper.js
@@ -0,0 +1,42 @@
+const _querySearchKey = "querySearch";
+
+/// for interop
+function getColor() {
+    let search = localStorage.getItem(_querySearchKey) ?? document.location.search;
+
+    return getQueryParam(search, 'color');
+}
+
+function changeFavIcon(src) {
+    var link = document.createElement('link'),
+        oldLink = document.getElementById('dynamic-favicon');
+    link.id = 'dynamic-favicon';
+    link.rel = 'shortcut icon';
+    link.href = src;
+    if (oldLink) {
+        document.head.removeChild(oldLink);
+    }
+    document.head.appendChild(link);
+}
+
+function handleInitialSearch() {
+    let search = document.location.search;
+
+    if (search == '') return;
+
+    localStorage.setItem(_querySearchKey, search);
+}
+
+function getLocalSearch() {
+    let search = localStorage.getItem(_querySearchKey);
+
+    return search;
+
+}
+
+
+function getQueryParam(search, name) {
+    let params = new URLSearchParams(search);
+
+    return params.get(name);
+}
--- canvas/script/loading.js
+++ canvas/script/loading.js
@@ -1,16 +1,31 @@
 window.addEventListener('load', function (ev) {
-       let loading = document.querySelector('#icon');
-       let bottom = document.querySelector('#bottom');
-       _flutter.loader.loadEntrypoint({
-         serviceWorker: {
-           serviceWorkerVersion: serviceWorkerVersion,
-         }
-       }).then(function (engineInitializer) {
-         return engineInitializer.initializeEngine();
-       }).then(function (appRunner) {
-        return appRunner.runApp().then(function () {
-        loading.remove();
-        bottom.remove();
-        });
-       });
-     });
+  let loading = document.querySelector('#icon');
+  let bottom = document.querySelector('#bottom');
+
+  _init();
+
+  _flutter.loader.loadEntrypoint({
+    serviceWorker: {
+      serviceWorkerVersion: serviceWorkerVersion,
+    }
+  }).then(function (engineInitializer) {
+    return engineInitializer.initializeEngine();
+  }).then(function (appRunner) {
+    return appRunner.runApp().then(function () {
+      loading.remove();
+      bottom.remove();
+    });
+  });
+});
+
+
+function _init() {
+  handleInitialSearch();
+
+  let search = getLocalSearch() ?? document.location.search;
+  let iconUrl = getQueryParam(search, 'iconUrl');
+
+  if (iconUrl) changeFavIcon(iconUrl);
+}
+
+
